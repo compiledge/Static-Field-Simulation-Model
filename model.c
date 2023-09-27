9347eaea02af2dd386368b39f4afb7d91810a5f2
compiledge <eduardo.iftm@gmail.com>
2023-09-27 17:09:40 -0300

    fix: Update the help menu

--- model.c
+++ model.c
@@ -3,24 +3,28 @@
 
 // help_menu() {{{
 void help_menu(){
-	printf("\t\t\t SIMULATION\n");
+	printf("\t\tSIMULATION\n");
 	printf("\nUSAGE:\n");
 	printf("\t static [options]\n");
 
-	printf("\nMODEL OPTIONS:\n");
-	printf("\t-e, --export\t\t Exportar matriz de fields em .csv\n");
-	printf("\t-l, --lambda <int>\t Valor de lambda para valor das diagonais\n");
-	printf("\t-p, --panic <int>\t Probabilidade de pedestre entrar em pânico\n");
-	printf("\t-r, --random <int>\t Quantidade de pedestres inseridos aleatoriamente\n");
-	printf("\t-s, --seed <int>\t Valor do seed para os sorteios\n");
-	printf("\t-S, --steps <int>\t Quantidade de passos na simulação\n");
-	printf("\t-m, --map <path>\t Arquivo padronizado de mapa.\n");
+	printf("\nFLAG OPTIONS:\n");
+	printf("\t-c, --curve\t\t Get around obstacles\n");
+	printf("\t-d, --dump\t\t Export the dump file\n");
+	printf("\t-e, --emoji\t\t Display emojis\n");
+	printf("\t-g, --greed\t\t Greedy behaviour\n");
+	printf("\t-n, --no-ex\t\t Prevent useless diagonal movements\n");
+	printf("\t-v, --verbose\t\t Display the entire simulation\n");
+
+	printf("\nINPUT OPTIONS:\n");
+	printf("\t-l, --lambda <int>\t Diagonal rate\n");
+	printf("\t-p, --panic <int>\t Panic rate\n");
+	printf("\t-r, --random <int>\t Number of random pedestrians inserted\n");
+	printf("\t-s, --seed <int>\t Seed value\n");
+	printf("\t-S, --steps <int>\t Number of time steps\n");
+	printf("\t-m, --map <path>\t Input map\n");
 
 	printf("\nMETA OPTIONS:\n");
-	printf("\t-v, --verbose\t\t Imprimir informações adicionais\n");
-	printf("\t-V, --version\t\t Imprimir a versão do programa\n");
-	printf("\t-h, --help\t\t Imprimir menu de ajuda com opções\n");
+	printf("\t-V, --version\t\t Software version\n");
+	printf("\t-h, --help\t\t Print this menu\n");
 
-	printf("\nEXAMPLE:\n");
-	printf("./static --lambda 150 --panic 5 --random 3 --seed 0 --steps 77 --map exp/double-corridor.in\n");
 }/*}}}*/
